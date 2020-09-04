c11a59d59bc62b32d403455caed857968ff8eeea
francesco <francesco.gadaleta@gmail.com>
2020-09-04 08:40:59 +0200

    WIP encoder for generic data types

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,8 @@ pub enum EncoderType {
     CustomMapping
 }
 
+/// Transformed data type
+///
 #[derive(Debug, Clone)]
 pub enum Transform {
     Ordinal(Vec<u64>),
@@ -40,12 +42,12 @@ where T: Debug + Eq + Hash + Clone
         }
     }
 
-
+    /// Fit label encoder given the type (ordinal, one-hot, custom)
+    ///
     pub fn fit(&mut self, data: &Vec<T>) {
         match self {
             Encoder2::Ordinal(map) => {
                 let mut current_idx = 0u64;
-
                 for el in data.iter() {
                     if !map.contains_key(el) {
                         map.insert(el.clone(), current_idx);
@@ -53,12 +55,13 @@ where T: Debug + Eq + Hash + Clone
                     }
                 }
             },
-
             _ => unimplemented!(),
 
         }
     }
 
+    /// Transform data to normalized encoding
+    ///
     pub fn transform(&self, data: &Vec<T>) -> Transform {
         match self {
             Encoder2::Ordinal(map) => {
@@ -69,113 +72,39 @@ where T: Debug + Eq + Hash + Clone
                             .collect();
                             Transform::Ordinal(res)
                     },
-
             _ => unimplemented!()
         }
     }
 
+    /// Transform labels back to original encoding.
+    ///
     pub fn inverse_transform(&self, data: &Transform) -> Transform {
         match self {
             Encoder2::Ordinal(map) => {
                 match data {
-                    Transform::Ordinal(t) => {
-                        map
-                        .iter()
-                        .find_map(|(k, &val)| if val == *el { Some(k)} else { None } )
-                    }
-                }
+                    Transform::Ordinal(typed_data) => {
+                            let map = map.clone();
+                            let result = typed_data.into_iter()
+                                    .filter_map(|el| map.iter()
+                                            .find_map(|(key, &val)| if val == *el { Some(key) } else { None })
+                                            .cloned()
+                                            .collect());
 
-                let res: Vec<T> = data.iter().filter_map(|el| {
-                            match el {
-                                Transform::Ordinal(t) => {
-                                    map
-                                    .iter()
-                                    .find_map(|(k, &val)| if val == *el { Some(k)} else { None } )
-                                    },
-                                _ => unimplemented!()
-                            }
-
-                //             map.iter()
-                //             .find_map(|(key, &val)| if val == *el { Some(key) } else { None })
-                //         })
-                //         .cloned()
-                //         .collect();
-                // res
-
+                            // let something: Vec<u64> = result.into_iter();
+                            Transform::Ordinal(result)
                     },
-            _ => unimplemented!()
-            }
-    }
 
-// #[derive(Debug, Clone)]
-// pub struct Encoder<T, U>
-// where T: Eq + Hash
-// {
-//     pub mapping: HashMap<T, U>,
-//     pub enctype: EncoderType,
-// }
-// impl <T, U> Encoder<T, U>
-// where T: Eq + Hash + Clone,
-//       U: Clone + PartialEq
-// {
-//     pub fn new(enctype: Option<EncoderType>) -> Encoder<T, U> {
-//         let enctype = enctype.unwrap_or(EncoderType::Ordinal);
-//         let mapping: HashMap<T, U> = HashMap::new();
-//         Encoder {
-//             mapping: mapping,
-//             enctype: enctype
-//         }
-//     }
-//     /// Fit label encoder given the type (ordinal, one-hot, custom)
-//     ///
-//     pub fn fit(&self, data: &Vec<T>) -> Encoder<T, U> {
-//         let mut mapping: HashMap<T, U> = HashMap::new();
-//         match self.enctype {
-//             EncoderType::Ordinal => {
-//                 let mut current_idx = 0u64;
-//                 // HashMap<T, u64>
-//                 for el in data.iter() {
-//                     if !mapping.contains_key(el) {
-//                         mapping.insert(el.clone(), current_idx );
-//                         current_idx +=1;
-//                     }
-//                 }
-//             },
-//             _ => unimplemented!()
-//         }
-//         Encoder {
-//             mapping: mapping,
-//             enctype: self.enctype.clone(),
-//         }
-//     }
-//     pub fn nclasses(&self) -> usize {
-//         self.mapping.len()
-//     }
-//     /// Return the unique labels
-//     pub fn uniques(&self) -> Vec<T> {
-//         self.mapping.keys().cloned().collect()
-//     }
-//     /// Transform data to normalized encoding
-//     ///
-//     pub fn transform(&self, data: &Vec<T>) -> Vec<U> {
-//         // for each element in data, get the value at mapping[element]
-//         data.iter()
-//             .filter_map(|el| self.mapping.get(el))
-//             .cloned()
-//             .collect()
-//     }
-//     /// Transform labels back to original encoding.
-//     ///
-//     pub fn inverse_transform(&self, data: &Vec<U>) -> Vec<T> {
-//         data.iter()
-//             .filter_map(|el| {
-//                 self.mapping.iter()
-//                 .find_map(|(key, &val)| if val == *el { Some(key) } else { None })
-//             })
-//             .cloned()
-//             .collect()
-//     }
-// }
+                    Transform::OneHot(t) => panic!("Transformed data not compatible with this encode");
+                }
+            },
+
+            Encoder2::OneHot(map) => unimplemented!(),
+
+            Encoder2::CustomMapping(map) => unimplemented!(),
+        }
+    }
+}
+
 
 
 #[cfg(test)]
@@ -197,18 +126,13 @@ mod tests {
         let mut enc: Encoder2<String> = Encoder2::Ordinal(HashMap::new());
         dbg!("encoder: ", &enc);
         let _ = enc.fit(&data);
-
         // dbg!(enc.mapping.clone());
         // assert_eq!(enc.nclasses(), 4);
-
         let trans_data = enc.transform(&data);
         dbg!(trans_data.clone());
-
         // let recon_data = enc.inverse_transform(&trans_data);
         // dbg!(recon_data.clone());
-
         // assert_eq!(recon_data.len(), 9);
-
         // let uniques = enc.uniques();
         // dbg!("uniques: ", uniques);
     }
