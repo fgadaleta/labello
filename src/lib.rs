b7224c94515680a339d860b202084e45969e587d
Francesco Gadaleta <francesco@amethix.com>
2020-09-05 14:48:37 +0200

    WIP passing configuration to fit()

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,26 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::iter::Iterator;
+use std::default::Default;
 
+
+// use rayon::prelude::*;
+// use std::iter::Sum;
+// use std::ops::Mul;
+// fn sum_of_squares<T: Send + Sync + Sum + Mul>(input: &Vec<T>) -> T
+// {
+//     input.par_iter()
+//          .map(|&i| i * i)
+//          .sum()
+// }
+
+// const fn num_bits<T>() -> usize {
+//     std::mem::size_of::<T>() * 8
+// }
+// fn log_2(x: i32) -> u32 {
+//     assert!(x > 0);
+//     num_bits::<i32>() as u32 - x.leading_zeros() - 1
+// }
 
 #[derive(Debug, Clone)]
 pub enum EncoderType {
@@ -14,6 +33,26 @@ pub enum EncoderType {
     // user-defined mapping function
     CustomMapping,
 }
+
+// TODO to replace current enctype in new()
+#[derive(Debug, Clone)]
+pub struct Config {
+    // maximum number of classes (repeat after the max)
+    pub max_nclasses: Option<u64>,
+    // only for custom encoder
+    pub mapping_function: Option<fn(String) -> u64>,
+}
+
+fn foo(config: &Config) {
+
+}
+
+// fn call_twice<A, F>(val: A, mut f: F) -> A
+// where F: FnMut(A) -> A {
+//     let tmp = f(val);
+//     f(tmp)
+// }
+
 
 /// Transformed data type
 ///
@@ -31,7 +70,7 @@ where
 {
     Ordinal(HashMap<T, u64>),
     OneHot(HashMap<T, Vec<bool>>),
-    CustomMapping(HashMap<T, String>),
+    Custom(HashMap<T, String>),
 }
 
 impl<T> Encoder2<T>
@@ -39,6 +78,7 @@ where
     T: Debug + Eq + Hash + Clone + Send + Sync,
 {
     pub fn new(enctype: Option<EncoderType>) -> Encoder2<T> {
+
         let enctype = enctype.unwrap_or(EncoderType::Ordinal);
 
         match enctype {
@@ -50,6 +90,14 @@ where
         }
     }
 
+    // Clean me
+    // pub fn set_mapping(&self, func: impl Fn(T) -> T) {
+    //     match self {
+    //         Encoder2::Custom(map) => unimplemented!(),
+    //         _ => panic!("Can set custom mapping only for EncoderType::CustomMapping.")
+    //     }
+    // }
+
     /// Return number of unique categories
     ///
     pub fn nclasses(&self) -> usize {
@@ -57,13 +105,16 @@ where
             // TODO len is the same for every type
             Encoder2::Ordinal(map) => map.len(),
             Encoder2::OneHot(map) => map.len(),
-            Encoder2::CustomMapping(map) => map.len(),
+            Encoder2::Custom(map) => map.len(),
         }
     }
 
     /// Fit label encoder given the type (ordinal, one-hot, custom)
     ///
-    pub fn fit(&mut self, data: &Vec<T>) {
+    pub fn fit(&mut self, data: &Vec<T>, config: &Config) {
+        // TODO integrate config and take max_nclasses and mapping from there
+        let _ = config;
+
         let datalen = data.len();
         dbg!("datalen ", datalen);
 
@@ -88,13 +139,6 @@ where
                         current_idx += 1;
                     }
                 }
-
-                // let _ = data.par_iter().map(|el| {
-                //     if !mapping.contains_key(el) {
-                //         mapping.insert(el.clone(), current_idx);
-                //         current_idx += 1;
-                //     }
-                // });
 
                 // create a vector of as many elements as unique categories
                 let vecsize = mapping.len();
@@ -125,9 +169,18 @@ where
                 }
             }
 
-            Encoder2::CustomMapping(map) => unimplemented!(),
+            Encoder2::Custom(map) => {
+                // let mapping_function = match mapping_func {
+                //     Some(mf) => mf,
+                //     None => panic!("Define a mapping function for a Custom encoder")
+                // };
+                // dbg!("User-defined mapping function ", mapping_function);
+                unimplemented!();
+            }
+
         }
     }
+
 
     /// Transform data to normalized encoding
     ///
@@ -141,7 +194,7 @@ where
 
             Encoder2::OneHot(_map) => unimplemented!(),
 
-            Encoder2::CustomMapping(_map) => unimplemented!(),
+            Encoder2::Custom(_map) => unimplemented!(),
         }
     }
 
@@ -168,7 +221,7 @@ where
 
             Encoder2::OneHot(_map) => unimplemented!(),
 
-            Encoder2::CustomMapping(_map) => unimplemented!(),
+            Encoder2::Custom(_map) => unimplemented!(),
         }
     }
 }
@@ -214,13 +267,18 @@ mod tests {
         let mut enc: Encoder2<String> = Encoder2::new(Some(enctype));
         dbg!("created encoder", &enc);
 
-        enc.fit(&data);
+        // provide configuation for fitting
+        let config = Config {
+            max_nclasses: Some(10),
+            mapping_function: None,
+        };
+        enc.fit(&data, &config);
         dbg!("fitted encoder", &enc);
 
         let trans_data = enc.transform(&data);
         dbg!("transformed data", &trans_data);
 
-        let reco_data = enc.inverse_transform(&trans_data);
+        let reco_data: Vec<String> = enc.inverse_transform(&trans_data);
         dbg!("reconstructed data", reco_data);
         assert_eq!(enc.nclasses(), 4);
     }
@@ -241,7 +299,54 @@ mod tests {
         let enctype = EncoderType::OneHot;
         let mut enc: Encoder2<String> = Encoder2::new(Some(enctype));
         dbg!("created encoder", &enc);
-        enc.fit(&data);
+
+        let config = Config {
+            max_nclasses: Some(10),
+            mapping_function: Some(
+                    |_el| {
+                        match _el.as_str() {
+                            "" => 42,
+                            _ => 0
+                        }
+                }),
+        };
+
+        enc.fit(&data, &config);
         dbg!("fitted encoder", &enc);
+    }
+
+    #[test]
+    fn test_fit_custom_encoder() {
+        let data: Vec<String> = vec![
+            "hello".to_string(),
+            "world".to_string(),
+            "world".to_string(),
+            "world".to_string(),
+            "world".to_string(),
+            "again".to_string(),
+            "hello".to_string(),
+            "again".to_string(),
+            "goodbye".to_string(),
+        ];
+
+        let config = Config {
+            max_nclasses: Some(10),
+            mapping_function: Some(
+                    |_el| {
+                        match _el.as_str() {
+                            "" => 42,
+                            _ => 0
+                        }
+                }),
+        };
+        dbg!("config max_nclasses: {}",  &config.max_nclasses);
+
+        let enctype = EncoderType::CustomMapping;
+        let mut enc: Encoder2<String> = Encoder2::new(Some(enctype));
+        dbg!("created encoder", &enc);
+
+        // enc.fit(&data);
+        // dbg!("fitted encoder", &enc);
+
     }
 }
